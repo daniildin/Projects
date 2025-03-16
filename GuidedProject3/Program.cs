4d74ec426aa94d1e8a240cb455c0fe3c56f9737c
daniilnsdk <daniilnsdk@gmail.com>
2025-03-16 14:04:03 -0400

    pseudo-code and methods

---
+++ GuidedProject3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+string[] pettingZoo = 
+{
+    "alpacas", "capybaras", "chickens", "ducks", "emus", "geese", 
+    "goats", "iguanas", "kangaroos", "lemurs", "llamas", "macaws", 
+    "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
+};
+
+/*- There will be three visiting schools
+    - School A has six visiting groups (the default number)
+    - School B has three visiting groups
+    - School C has two visiting groups
+
+- For each visiting school, perform the following tasks
+    - Randomize the animals
+    - Assign the animals to the correct number of groups
+    - Print the school name
+    - Print the animal groups*/
+
+
+// RandomizeAnimals();
+// string[,] group = AssignGroup();
+Console.WriteLine("School A");
+// PrintGroup(group);
