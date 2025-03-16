82b0d04c09da27fbea7feaa09e38dfe8f0c92a88
daniilnsdk <daniilnsdk@gmail.com>
2025-03-16 14:09:52 -0400

    added method to shuffle an array

--- GuidedProject3/obj/Debug/net9.0/GuidedProject3.AssemblyInfo.cs
+++ GuidedProject3/obj/Debug/net9.0/GuidedProject3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GuidedProject3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a92466394155f2da60d13197573180999f43ca15")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d74ec426aa94d1e8a240cb455c0fe3c56f9737c")]
 [assembly: System.Reflection.AssemblyProductAttribute("GuidedProject3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GuidedProject3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
