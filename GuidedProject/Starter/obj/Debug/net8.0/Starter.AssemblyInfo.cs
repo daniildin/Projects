6600cf23f18908a8784ab23872dc1dd138841f30
daniilnsdk <daniilnsdk@gmail.com>
2025-03-16 09:31:03 -0400

    added loop to test new pet data

--- GuidedProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ GuidedProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a92466394155f2da60d13197573180999f43ca15")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7a0aad8d608c2569ed9b2f1e2629d9114945e71")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
