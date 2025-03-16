daf916b3271de24e209f7da24c990c3900497291
daniilnsdk <daniilnsdk@gmail.com>
2025-03-16 14:51:52 -0400

    make the player consume food and change apperance

--- Starter/Program.cs
+++ Starter/Program.cs
@@ -15,8 +15,8 @@ int foodX = 0;
 int foodY = 0;
 
 // Available player and food strings
-string[] states = {"('-')", "(^-^)", "(X_X)"};
-string[] foods = {"@@@@@", "$$$$$", "#####"};
+string[] states = { "('-')", "(^-^)", "(X_X)" };
+string[] foods = { "@@@@@", "$$$$$", "#####" };
 
 // Current player string displayed in the Console
 string player = states[0];
@@ -25,9 +25,9 @@ string player = states[0];
 int food = 0;
 
 InitializeGame();
-while (!shouldExit) 
+while (!shouldExit)
 {
-    if (TerminalResized()) 
+    if (TerminalResized())
     {
         Console.Clear();
         Console.WriteLine("Console was resized. Program exiting.");
@@ -35,66 +35,64 @@ while (!shouldExit)
         break;
     }
     Move(true);
+    if (PlayerConsumedFood())
+    {
+        ShowFood();  // First, spawn new food
+        ChangePlayer(); // Then change player
+    }
 }
 
-// Returns true if the Terminal was resized 
-bool TerminalResized() 
+// Returns true if the Terminal was resized
+bool TerminalResized()
 {
     return height != Console.WindowHeight - 1 || width != Console.WindowWidth - 5;
 }
 
 // Displays random food at a random location
-void ShowFood() 
+void ShowFood()
 {
     food = random.Next(0, foods.Length);
-    foodX = random.Next(0, width - player.Length);
-    foodY = random.Next(0, height - 1);
+    foodX = random.Next(1, width - player.Length);
+    foodY = random.Next(1, height - 1);
     Console.SetCursorPosition(foodX, foodY);
     Console.Write(foods[food]);
 }
 
 // Changes the player to match the food consumed
-void ChangePlayer() 
+void ChangePlayer()
 {
     player = states[food];
     Console.SetCursorPosition(playerX, playerY);
     Console.Write(player);
 }
 
-// Temporarily stops the player from moving
-void FreezePlayer() 
-{
-    System.Threading.Thread.Sleep(1000);
-    player = states[0];
-}
-
 // Reads directional input from the Console and moves the player
-void Move(bool allowExitOnOtherKeys = false) 
+void Move(bool allowExitOnOtherKeys = false)
 {
     int lastX = playerX;
     int lastY = playerY;
-    
+
     ConsoleKey key = Console.ReadKey(true).Key;
-    
-    switch (key) 
+
+    switch (key)
     {
         case ConsoleKey.UpArrow:
-            playerY--; 
+            playerY--;
             break;
-        case ConsoleKey.DownArrow: 
-            playerY++; 
+        case ConsoleKey.DownArrow:
+            playerY++;
             break;
-        case ConsoleKey.LeftArrow:  
-            playerX--; 
+        case ConsoleKey.LeftArrow:
+            playerX--;
             break;
-        case ConsoleKey.RightArrow: 
-            playerX++; 
+        case ConsoleKey.RightArrow:
+            playerX++;
             break;
-        case ConsoleKey.Escape:     
-            shouldExit = true; 
+        case ConsoleKey.Escape:
+            shouldExit = true;
             break;
         default:
-            if (allowExitOnOtherKeys) 
+            if (allowExitOnOtherKeys)
             {
                 shouldExit = true;
             }
@@ -103,10 +101,7 @@ void Move(bool allowExitOnOtherKeys = false)
 
     // Clear the previous position
     Console.SetCursorPosition(lastX, lastY);
-    for (int i = 0; i < player.Length; i++) 
-    {
-        Console.Write(" ");
-    }
+    Console.Write(new string(' ', player.Length));
 
     // Keep player within bounds
     playerX = Math.Clamp(playerX, 0, width);
@@ -117,8 +112,14 @@ void Move(bool allowExitOnOtherKeys = false)
     Console.Write(player);
 }
 
+// Determines if the player has consumed the food
+bool PlayerConsumedFood()
+{
+    return playerX >= foodX && playerX < foodX + foods[food].Length && playerY == foodY;
+}
+
 // Clears the console, displays the food and player
-void InitializeGame() 
+void InitializeGame()
 {
     Console.Clear();
     ShowFood();
--- Starter/obj/Debug/net6.0/Starter.AssemblyInfo.cs
+++ Starter/obj/Debug/net6.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a92466394155f2da60d13197573180999f43ca15")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e19ab4b072d85221c45dddd4626e5decc134a51c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
